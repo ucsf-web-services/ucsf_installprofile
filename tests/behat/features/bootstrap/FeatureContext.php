a7a1f17191c1836521e1f4a4b15e2067373a5f01
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2014-10-27 19:18:05 -0700

    implemented step definitions. added kludge to make logged-in check pass.

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -1,17 +1,53 @@
 <?php
 use Behat\Behat\Exception\PendingException;
 use Drupal\DrupalExtension\Context\DrupalContext;
+use Behat\Behat\Event\SuiteEvent;
+
+
+require_once 'PHPUnit/Autoload.php';
+require_once 'PHPUnit/Framework/Assert/Functions.php';
 
 /**
  * Features context.
  */
 class FeatureContext extends DrupalContext
 {
-    /**
-     * @Given /^I am on the home page$/
+
+
+  /**
+   * @BeforeSuite
+   */
+  public static function prepare(SuiteEvent $event)
+  {
+    /*
+     * Kludge!
+     * see https://www.drupal.org/node/2023625#comment-8607207
+     * [ST 2014/10/27]
      */
-    public function iAmOnTheHomePage()
-    {
-        $this->assertHomepage();
-    }
+    variable_set('admin_menu_cache_client', FALSE);
+  }
+
+  /**
+   * @Given /^I am on the home page$/
+   */
+  public function iAmOnTheHomePage()
+  {
+    $this->assertHomepage();
+  }
+
+  /**
+   * @Then /^I should have the "([^"]*)" permission$/
+   */
+  public function iShouldHaveThePermission($permission) {
+    $user = user_load($this->user->uid);
+    assertTrue(user_access($permission, $user));
+  }
+
+  /**
+   * @Then /^I should not have the "([^"]*)" permission$/
+   */
+  public function iShouldNotHaveThePermission($permission) {
+    $user = user_load($this->user->uid);
+    assertFalse(user_access($permission, $user));
+  }
 }
