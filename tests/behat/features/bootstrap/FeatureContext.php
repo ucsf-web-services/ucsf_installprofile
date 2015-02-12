61571de1ff8e2d69a87dc45b28e164a3a96d04cd
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-02-12 15:38:38 -0800

    yanked obsolete test scenarios and step definitions.

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -48,92 +48,11 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
   }
 
   /**
-   * @Then /^I should have the "([^"]*)" permission$/
-   *
-   * @depecated
-   * @todo Broken in BH3 b/c we can't access current user anymore. Find a suitable replacement [ST 2015/02/12].
-   */
-  public function iShouldHaveThePermission($permission) {
-    $user = NULL;
-    if ($this->user) {
-      $user = user_load($this->user->uid);
-    }
-    assertTrue(user_access($permission, $user));
-  }
-
-  /**
-   * @Then /^I should have the "([^"]*)" permissions$/
-   *
-   * @depecated
-   * @todo Broken in BH3 b/c we can't access current user anymore. Find a suitable replacement [ST 2015/02/12].
-   */
-  public function iShouldHaveThePermissions($permissions) {
-    $user = NULL;
-    if ($this->user) {
-      $user = user_load($this->user->uid);
-    }
-    $permissions = explode(',', $permissions);
-    foreach ($permissions as $permission) {
-      assertTrue(user_access(trim($permission), $user),
-        sprintf("The current user does not have the %s permission.", $permission));
-    }
-  }
-
-  /**
-   * @Then /^I should not have the "([^"]*)" permission$/
-   *
-   * @depecated
-   * @todo Broken in BH3 b/c we can't access current user anymore. Find a suitable replacement [ST 2015/02/12].
-   */
-  public function iShouldNotHaveThePermission($permission) {
-    $user = NULL;
-    if ($this->user) {
-      $user = user_load($this->user->uid);
-    }
-    assertFalse(user_access($permission, $user));
-  }
-
-
-  /**
    * @Then /^I should see the admin menu$/
    */
   public function iShouldSeeTheAdminMenu() {
     $regionObj = $this->getRegion("Admin Menu");
     assertTrue($regionObj->isVisible());
-  }
-
-  /**
-   * @Then /^I should have full permissions$/
-   *
-   * @depecated
-   * @todo Broken in BH3 b/c we can't access current user anymore. Find a suitable replacement [ST 2015/02/12].
-   */
-  public function iShouldHaveFullPermissions() {
-
-    // get all permissions
-    $all_permissions = module_invoke_all('permission');
-    $all_permission_names = array_keys($all_permissions);
-    sort($all_permission_names);
-
-    // get all permissions for the current user's role
-    $role_name = $this->user->role;
-    $role = user_role_load_by_name($role_name);
-    $user_permissions = user_role_permissions(array($role->rid => $role->name));
-    $user_permission_names = array_keys($user_permissions[$role->rid]);
-    sort($user_permission_names);
-
-    // compare all perms with admin perms
-    $delta = array_diff($all_permission_names, $user_permission_names);
-    sort($delta);
-
-    // perms that are assumed to always be disabled
-    $disabled_perms = array();
-
-    // Webedit module yanks this permission on flush caches
-    $disabled_perms[] = 'use text format webedit_paste';
-
-    // there should only be disabled perms left at this point
-    assertEquals($disabled_perms, $delta);
   }
 
   /**
